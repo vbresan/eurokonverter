064a753e58549d897cb3e312e1801a3a35ea634d
vbresan <288855+vbresan@users.noreply.github.com>
2023-10-07 09:03:41 +0200

    Migracija web stranica

---
+++ docs/js/gallery.js
@@ -0,0 +1,27 @@
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	
+	let i;
+  	let slides = document.getElementsByClassName("screenshot");
+  	
+  	if (n > slides.length) {
+		slideIndex = 1
+	}
+  	if (n < 1) {
+		slideIndex = slides.length
+	}
+	
+  	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+  	}
+  	
+  	slides[slideIndex - 1].style.display = "block";
+  	document.getElementById("slideNumber").innerText = slideIndex + " / " + slides.length;
+}
