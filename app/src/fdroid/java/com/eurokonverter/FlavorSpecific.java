8ec291367e6af1192cf303d5c5ed5979c6af0b03
vbresan <288855+vbresan@users.noreply.github.com>
2025-07-14 15:08:28 +0200

    Adding privacy policy. Contrast enhancement.

---
+++ app/src/fdroid/java/com/eurokonverter/FlavorSpecific.java
@@ -0,0 +1,17 @@
+package com.eurokonverter;
+
+import android.app.Activity;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+public class FlavorSpecific {
+
+    public FlavorSpecific(Activity activity) {
+        // do nothing
+    }
+
+    public void enablePrivacyPolicyLink() {
+        // do nothing
+    }
+}
--- app/src/main/java/com/eurokonverter/ShareActivity.java
+++ app/src/main/java/com/eurokonverter/ShareActivity.java
@@ -15,6 +15,8 @@ public class ShareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        new FlavorSpecific(this).enablePrivacyPolicyLink();
     }
 
     /**
