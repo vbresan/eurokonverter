61226a2b62ec94675c2882c006d66c53a437e6ec
vbresan <288855+vbresan@users.noreply.github.com>
2025-07-14 15:54:24 +0200

    Merging the class

--- app/src/gplay/java/com/eurokonverter/FlavorSpecific.java
+++ app/src/gplay/java/com/eurokonverter/FlavorSpecific.java
@@ -3,6 +3,7 @@ package com.eurokonverter;
 import android.app.Activity;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
 public class FlavorSpecific {
@@ -19,5 +20,27 @@ public class FlavorSpecific {
         if (textView != null) {
             textView.setMovementMethod(LinkMovementMethod.getInstance());
         }
+    }
+
+    public void showAd() {
+
+        View view = activity.findViewById(R.id.frameLayoutImageHeader);
+        if (view == null) {
+            return;
+        }
+
+        view.getViewTreeObserver().addOnGlobalLayoutListener(
+            new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+
+                    int heightPx = view.getHeight();
+                    int heightDp = (int) (heightPx / activity.getResources().getDisplayMetrics().density);
+                    System.out.println("===============> heightPx: " + heightPx);
+                    System.out.println("===============> heightDp: " + heightDp);
+
+                    view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+            });
     }
 }
