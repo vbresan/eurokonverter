2ac8e49d4d466880fc7313f9c58fd247546c9cc6
vbresan <288855+vbresan@users.noreply.github.com>
2025-07-15 10:26:38 +0200

    Showing adaptive banner ad

--- app/src/gplay/java/com/eurokonverter/FlavorSpecific.java
+++ app/src/gplay/java/com/eurokonverter/FlavorSpecific.java
@@ -2,13 +2,50 @@ package com.eurokonverter;
 
 import android.app.Activity;
 import android.text.method.LinkMovementMethod;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class FlavorSpecific {
 
+    private static final int MIN_AD_HEIGHT = 50;
+    private static final int MIN_AD_PADDING = 32;
+    private static final int MIN_AD_REQUIRED_SPACE =
+        MIN_AD_HEIGHT + MIN_AD_PADDING;
+
     private final Activity activity;
+
+    private void doShowAd(FrameLayout parent, int widthDp, int heightDp) {
+
+        AdSize adSize = AdSize.getInlineAdaptiveBannerAdSize(widthDp, heightDp);
+        AdView bannerView = new AdView(activity);
+        bannerView.setAdUnitId(activity.getString(R.string.admob_ad_id));
+        bannerView.setAdSize(adSize);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        bannerView.loadAd(adRequest);
+
+        LinearLayout adContainer = new LinearLayout(activity);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        params.gravity = Gravity.TOP;
+
+        adContainer.setLayoutParams(params);
+        adContainer.setOrientation(LinearLayout.VERTICAL);
+
+        adContainer.addView(bannerView);
+        parent.addView(adContainer);
+    }
 
     public FlavorSpecific(Activity activity) {
         this.activity = activity;
@@ -24,22 +61,51 @@ public class FlavorSpecific {
 
     public void showAd() {
 
-        View view = activity.findViewById(R.id.frameLayoutImageHeader);
+        if (!activity.getResources().getBoolean(R.bool.show_ads)) {
+            return;
+        }
+
+        FrameLayout view = activity.findViewById(R.id.frameLayoutImageHeader);
         if (view == null) {
             return;
         }
 
         view.getViewTreeObserver().addOnGlobalLayoutListener(
             new ViewTreeObserver.OnGlobalLayoutListener() {
+
+                private float getDensity() {
+                    return activity.getResources().getDisplayMetrics().density;
+                }
+
+                @SuppressWarnings("UnnecessaryLocalVariable")
+                private int getWidth(View view) {
+
+                    int widthPx = view.getWidth();
+                    int widthDp = (int) (widthPx / getDensity());
+
+                    return widthDp;
+                }
+
+                @SuppressWarnings("UnnecessaryLocalVariable")
+                private int getHight(View view) {
+
+                    int heightPx = view.getHeight();
+                    int heightDp = (int) (heightPx / getDensity());
+
+                    return heightDp;
+                }
+
                 @Override
                 public void onGlobalLayout() {
 
-                    int heightPx = view.getHeight();
-                    int heightDp = (int) (heightPx / activity.getResources().getDisplayMetrics().density);
-                    System.out.println("===============> heightPx: " + heightPx);
-                    System.out.println("===============> heightDp: " + heightDp);
-
                     view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                    int widthDp  = getWidth(view);
+                    int heightDp = getHight(view);
+
+                    if (heightDp >= MIN_AD_REQUIRED_SPACE) {
+                        doShowAd(view, widthDp, heightDp - MIN_AD_PADDING);
+                    }
                 }
             });
     }
